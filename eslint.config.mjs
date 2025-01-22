7ea5b0c3bb65766b2377c2ee9d8271e7f9d00f67
Konstantin Volik <volik-vml@mail.ru>
2025-01-22 21:46:30 +0300

    Ебка с линтером

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,25 @@
-import tseslint from "typescript-eslint";
+import typescriptEslint from "typescript-eslint";
 import eslint from "@eslint/js";
-import stylistic from "@stylistic/eslint-plugin";
+
 import eslintPluginVue from "eslint-plugin-vue";
 
-export default tseslint.config(
-    eslint.configs.recommended,
-    tseslint.configs.recommendedTypeChecked,
-    tseslint.configs.stylisticTypeChecked,
-    ...eslintPluginVue.configs["flat/recommended"],
+export default typescriptEslint.config(
     {
-        plugins: {
-            'typescript-eslint': tseslint.plugin,
-        },
+        ignores: ['**.d.ts', '**/coverage', '**/dist']
+    },
+    {
+        extends: [
+            eslint.configs.recommended,
+            ...typescriptEslint.configs.recommendedTypeChecked,
+            ...eslintPluginVue.configs['flat/recommended'],
+        ],
+        files: ['**/*.{ts,vue}'],
+
         languageOptions: {
-            parser: tseslint.parser,
+            sourceType: 'module',
             parserOptions: {
-                project: './tsconfig.app.json',
-                extraFileExtensions: ['.vue'],
-                sourceType: 'module',
+                parser: typescriptEslint.parser,
             }
         }
     }
-    // stylistic.configs.customize({
-    //   "quotes": "double",
-    //   "semi": true,
-    //   "object-curly-spacing": ["error", "always"],
-    //
-    // }),
 );
--- src/pages/tasks/tasks-view.vue
+++ src/pages/tasks/tasks-view.vue
@@ -1,6 +1,15 @@
-<script setup>
-import { ref, computed } from 'vue';
-import { format, startOfMonth, endOfMonth, startOfWeek, endOfWeek, eachDayOfInterval, addMonths, subMonths } from 'date-fns';
+<script setup lang="ts" >
+import {ref, computed} from 'vue';
+import {
+  format,
+  startOfMonth,
+  endOfMonth,
+  startOfWeek,
+  endOfWeek,
+  eachDayOfInterval,
+  addMonths,
+  subMonths
+} from 'date-fns';
 import SchedulerItem from "@pages/tasks/scheduler-item.vue";
 
 const targetDate = ref(new Date());
@@ -15,8 +24,8 @@ const monthNames = [
 
 const firstDayOfCurrentMonth = computed(() => startOfMonth(new Date(currentYear.value, currentMonth.value, 1)));
 const lastDayOfCurrentMonth = computed(() => endOfMonth(new Date(currentYear.value, currentMonth.value, 1)));
-const startOfCalendar = computed(() => startOfWeek(firstDayOfCurrentMonth.value, { weekStartsOn: 1 })); // Понедельник
-const endOfCalendar = computed(() => endOfWeek(lastDayOfCurrentMonth.value, { weekStartsOn: 1 }));
+const startOfCalendar = computed(() => startOfWeek(firstDayOfCurrentMonth.value, {weekStartsOn: 1})); // Понедельник
+const endOfCalendar = computed(() => endOfWeek(lastDayOfCurrentMonth.value, {weekStartsOn: 1}));
 
 const weeks = computed(() => {
   const weeksArray = [];
@@ -36,26 +45,33 @@ const weeks = computed(() => {
 })
 
 const daysInMonth = computed(() => {
-  const interval = eachDayOfInterval({ start: startOfCalendar.value, end: endOfCalendar.value });
+  const interval = eachDayOfInterval({start: startOfCalendar.value, end: endOfCalendar.value});
   return interval.map(day => ({
     date: day,
     tasks: []
   }));
 });
 
-function previousMonth() {
+const openYear = (date) => {
+ // Открываем компонент текущего годового календаря с 12 месяцами
+}
+const openWeek = (week: []) => {
+  // Открываем компонент недельного календаря присвоив в переменную для недели переданный аргумент
+}
+
+const previousMonth = () => {
   currentDate.value = subMonths(currentDate.value, 1);
   currentMonth.value = currentDate.value.getMonth();
   currentYear.value = currentDate.value.getFullYear();
 }
 
-function nextMonth() {
+const nextMonth = () => {
   currentDate.value = addMonths(currentDate.value, 1);
   currentMonth.value = currentDate.value.getMonth();
   currentYear.value = currentDate.value.getFullYear();
 }
 
-function selectDay(day) {
+const selectDay = (day) => {
   if (day) {
     console.log(`Выбран день: ${day}`);
   }
@@ -66,20 +82,41 @@ function selectDay(day) {
   <section>
     <h1>Tasks</h1>
     <article>
-      <div>{{ format(targetDate, 'MMMM') }}</div>
-      <div class="flex flex-col rounded border border-state-500 divide-y divide-state-500">
+
+      <div class="flex flex-col">
         <div class="flex justify-around">
-          <div v-for="day in daysOfWeek" :key="day" class="flex-1 p-2">
-            {{ day }}
+          <div class="w-1/12 flex items-center justify-center" @click="openYear">{{ format(targetDate, 'MMMM') }}</div>
+          <div class="flex border rounded-t-lg  w-11/12 divide-x divide-state-500">
+            <div v-for="day in daysOfWeek" :key="day" class=" flex-1 p-2">
+              {{ day }}
+            </div>
+          </div>
+
+        </div>
+        <div v-for="(week, index) in weeks" class="flex g-1  ">
+          <button class="w-1/12" @click="openWeek(week)">{{ `${index + 1} week` }}</button>
+          <div class="
+          flex justify-around flex-1
+          divide-x divide-state-500
+          border-state-500
+          border-x
+          border-collapse
+          "
+          :class="{
+            'border-t': index !== 0,
+            'border-b': index === weeks.length - 1,
+            'rounded-b-lg': index === weeks.length - 1
+
+          }">
+
+            <SchedulerItem
+                v-for="day in week"
+                :key="day"
+                :day="day"
+            />
           </div>
         </div>
-        <div class="flex justify-around" v-for="week in weeks">
-          <SchedulerItem
-              v-for="day in week"
-              :key="day"
-              :day="day"
-          />
-        </div>
+
 
       </div>
 
